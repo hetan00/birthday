cace0178bb2600a458718abd2ce65b8b09f30273
HetanKamalkumar <hetan_28@hotmail.com>
2025-11-17 16:39:28 +0000

    changed env

--- src/components/BirthdayScene.jsx
+++ src/components/BirthdayScene.jsx
@@ -93,7 +93,7 @@ function BirthdayScene() {
     <Environment
         background
         // files="/backgrounds/newyorknight.hdr"
-        files="/backgrounds/water-skyline.hdr"
+        files="/backgrounds/shanghai.hdr"
     />
 
     {/* Rotate the whole birthday setup */}
